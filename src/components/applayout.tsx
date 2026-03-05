3a09e1ebdaae1d01508d987ca06c560eb7ca1953
drdianehamilton <drdianehamilton@gmail.com>
2026-03-05 07:43:08 -0700

    Create applayout.tsx

---
+++ src/components/applayout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useAppContext } from '@/contexts/AppContext';
+import WelcomeScreen from './WelcomeScreen';
+import AssessmentScreen from './AssessmentScreen';
+import ResultsScreen from './ResultsScreen';
+
+const AppLayout: React.FC = () => {
+  const { currentStep } = useAppContext();
+
+  const renderCurrentStep = () => {
+    switch (currentStep) {
+      case 'welcome':
+        return <WelcomeScreen />;
+      case 'assessment':
+        return <AssessmentScreen />;
+      case 'results':
+        return <ResultsScreen />;
+      default:
+        return <WelcomeScreen />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen">
+      {renderCurrentStep()}
+    </div>
+  );
+};
+
+export default AppLayout;
