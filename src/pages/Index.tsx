09b2fe35bd83eb61549ffcf89bf309d0f3bea508
drdianehamilton <drdianehamilton@gmail.com>
2026-03-05 16:40:10 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import AppLayout from "@/components/AppLayout";
-import { AppProvider } from "@/contexts/AppContext";
+import AppLayout from "../components/AppLayout";
+import { AppProvider } from "../contexts/AppContext";
 
 const Index = () => {
   return (
