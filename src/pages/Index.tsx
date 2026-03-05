14784769eab912535fdf1028c966fe294c3e086a
drdianehamilton <drdianehamilton@gmail.com>
2026-03-05 15:29:55 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppLayout from '../components/ui/AppLayout';
+import AppLayout from '../components/AppLayout';
 import { AppProvider } from '../contexts/AppContext';
 
 const Index: React.FC = () => {
