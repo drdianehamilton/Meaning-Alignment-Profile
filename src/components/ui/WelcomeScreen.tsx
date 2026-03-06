30259d35a075e05080ec1239ea8ce20a7625e2e9
drdianehamilton <drdianehamilton@gmail.com>
2026-03-06 14:57:04 -0700

    Create WelcomeScreen.tsx

---
+++ src/components/ui/WelcomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeScreen = () => {
+  return (
+    <div style={{padding:40}}>
+      <h1 style={{fontSize:40}}>TEST PAGE</h1>
+      <p>If you see this, the correct component is loading.</p>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
