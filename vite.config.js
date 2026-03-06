ab8455f18ec3daaff65f4f7ea683b2079faa290a
drdianehamilton <drdianehamilton@gmail.com>
2026-03-06 05:58:41 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
+    host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react()
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
